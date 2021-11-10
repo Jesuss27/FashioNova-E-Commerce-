0d7b25325eba92216d2925d31da13c8ab0dc5286
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-11-10 15:06:15 -0800

    Final Code

--- control.js
+++ control.js
@@ -3,13 +3,30 @@ const navSlide = () => {
     const nav = document.querySelector(".links");
     const navLinks = document.querySelectorAll(".links li")
 
+
     hamburger.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
+        hamburger.classList.toggle("is-active");
         // animate nav links
         navLinks.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = ""
+            }else{
             link.style.animation =  `navFadeIn .5s ease forwards ${index / 7 + .3}s`
+            }
         })
+
     });
+
+    window.addEventListener("resize", () =>{
+        if(window.innerWidth >= 768){
+            nav.classList.remove("nav-active");
+            hamburger.classList.remove("is-active");
+            navLinks.forEach(link => {
+                link.style.animation = ""
+            })
+        }
+    })
 }
 
 navSlide(); 
